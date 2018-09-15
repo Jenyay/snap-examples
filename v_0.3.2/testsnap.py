1f28454043901f7c832d2f61593a3ddd929a3e00
Jenyay <jenyay.ilin@gmail.com>
2018-09-15 11:26:17 +0300

    First commit
    Versions 0.0.1 - 0.3.2

---
+++ v_0.3.2/testsnap.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import wx
+import wx.adv
+
+
+class MyTestFrame(wx.Frame):
+    def __init__(self, parent, title):
+        super().__init__(parent, wx.ID_ANY, title, size=(400, 150))
+        self._listCtrl = wx.ListCtrl(self, style=wx.LC_LIST)
+
+        for n in range(20):
+            self._listCtrl.InsertItem(0, 'bla-bla-bla-bla-bla')
+
+        self.Show()
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyTestFrame(None, 'Test')
+    app.MainLoop()
